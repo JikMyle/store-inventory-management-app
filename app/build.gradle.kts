ebccdef429644b181fc164b4a43d06a0c8adcfd9
JikMyle <itsmejimkyle@gmail.com>
2024-05-04 11:12:47 +0800

    Change App Icon
    This commit changes the app's icon provided by Andrei Lebadesus.
    This also defines the release and debug build type to be non-debuggable
    and debuggable respectively.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,11 +25,16 @@ android {
 
     buildTypes {
         release {
+            isDebuggable = false
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+        }
+
+        debug {
+            isDebuggable = true
         }
     }
     compileOptions {
